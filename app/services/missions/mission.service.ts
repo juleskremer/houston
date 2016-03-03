473ab3f42db273200c607f57e130b6039ef7af65
juleskremer <juleskremer@gmail.com>
2016-03-03 09:57:00 -0800

    removing

--- app/services/missions/mission.service.ts
+++ app/services/missions/mission.service.ts
@@ -12,37 +12,37 @@ export class MissionsService {
 
     getMission(id: string) {
 
-        var firebaseUrl: string;
-        var missionRef: Firebase;
-        var ngMission: Mission;
-
-        firebaseUrl = "https://ngmain.firebaseio.com/missions/" + id;
-        missionRef = new Firebase(firebaseUrl);
-
-        missionRef.once("value", function(snapshot) {
-            ngMission = snapshot.val();
-            // data === "hello"
-        });
-        return Promise.resolve(ngMission);
+//         var firebaseUrl: string;
+//         var missionRef: Firebase;
+//         var ngMission: Mission;
+// 
+//         firebaseUrl = "https://ngmain.firebaseio.com/missions/" + id;
+//         missionRef = new Firebase(firebaseUrl);
+// 
+//         missionRef.once("value", function(snapshot) {
+//             ngMission = snapshot.val();
+//             // data === "hello"
+//         });
+//         return Promise.resolve(ngMission);
 
     }
 
     getMissions() {
 
-        var firebaseUrl: string;
-        var missionsRef: Firebase;
-        var ngMissions: Mission[] = new Array();
-
-        firebaseUrl = "https://ngmain.firebaseio.com/missions";
-        missionsRef = new Firebase(firebaseUrl);
-
-        missionsRef.on("value", snapshot => {
-            snapshot.forEach(childSnap => {
-                ngMissions.push(childSnap.val())
-            });
-        });
-
-        return Promise.resolve(ngMissions);
+//         var firebaseUrl: string;
+//         var missionsRef: Firebase;
+//         var ngMissions: Mission[] = new Array();
+// 
+//         firebaseUrl = "https://ngmain.firebaseio.com/missions";
+//         missionsRef = new Firebase(firebaseUrl);
+// 
+//         missionsRef.on("value", snapshot => {
+//             snapshot.forEach(childSnap => {
+//                 ngMissions.push(childSnap.val())
+//             });
+//         });
+// 
+//         return Promise.resolve(ngMissions);
 
     }
 
